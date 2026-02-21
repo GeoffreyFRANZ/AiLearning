4ee5b373b2b70734bbf9ed06df3bbb94210dc892
geoffreyFRANZ <franz.geoffrey@hotmail.fr>
2026-02-21 23:39:43 +0100

    change CV  add  learning

--- classes/Activation_ReLU.py
+++ classes/Activation_ReLU.py
@@ -1,9 +1,9 @@
 import numpy as np
 class Activation_ReLU:
+
     def forward(self, inputs):
         self.inputs = inputs
         self.output = np.maximum(0, inputs)
-    def backward(self, d_in):
-        d_result = np.array(d_in, copy=True)
-        d_result[self.inputs <= 0] = 0
-        return d_result
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
-from classes import ( Layer_Dense, Activation_Softmax,
+from classes import (Layer_Dense, Activation_Softmax,
                      Activation_ReLU,Optimizer_SGD,
                      Activation_Softmax_Loss_CategoricalCrossentropy,
                      )
@@ -13,7 +13,7 @@ activation1 = Activation_ReLU()
 layer2 = Layer_Dense(64, 3)
 activation2 = Activation_Softmax()
 loss_activation_function = Activation_Softmax_Loss_CategoricalCrossentropy()
-optimizer = Optimizer_SGD(decay = 1e-2)
+optimizer = Optimizer_SGD(decay = 1e-3, momentum=0.9)
 for epoch in range(10001):
     layer1.forward(X)
     activation1.forward(layer1.output)
@@ -21,21 +21,20 @@ for epoch in range(10001):
     loss = loss_activation_function.forward(layer2.output, y)
 
     predictions = np.argmax(loss_activation_function.output, axis=1)
+    if len (y.shape) == 2:
+        y = np.argmax(y, axis=1)
     accuracy = np.mean(predictions == y)
-
-    dvalues = loss_activation_function.backward(loss_activation_function.output, y)
-    dvalues = layer2.backward(dvalues)
-    dvalues = activation1.backward(dvalues)
-    layer1.backward(dvalues)
-
-
-    optimizer.pre_update_params()
-    optimizer.update_params(layer1)
-    optimizer.update_params(layer2)
-    optimizer.post_update_params()
-
     if not epoch % 100:
         print(f'epoch: {epoch}, ' +
               f'acc: {accuracy:.3f}, ' +
               f'loss: {loss:.3f}, ' +
-              f'lr: {optimizer.current_learning_rate}')
+              f'lr: {optimizer.current_learning_rate}'
+              )
+    loss_activation_function.backward(loss_activation_function.output, y)
+    layer2.backward(loss_activation_function.d_inputs)
+    activation1.backward(layer2.dinputs)
+    layer1.backward(activation1.dinputs)
+    optimizer.pre_update_params()
+    optimizer.update_params(layer1)
+    optimizer.update_params(layer2)
+    optimizer.post_update_params()
